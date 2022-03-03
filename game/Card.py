d747b88a59d698c1d80f717484cf13c344c49df9
Luan Rexhepi <lu.rexhepi@gmail.com>
2022-03-03 01:36:59 +0100

    Added the code for the Card Class

--- game/Card.py
+++ game/Card.py
@@ -1,2 +1,16 @@
 # Assigned to Luan
-#A tset
+colors = ['Hearts','Clubs','Spades','Diamonds']
+
+cards = ['Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King','Ace']
+
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six' : 6 , 'Seven' :7, 'Eight': 8,'Nine':9,
+          'Ten':10,'Jack':11, 'Queen': 12,'King': 13,'Ace':14 }
+
+class Card:
+    def init(self, color, card):
+       self.color = color
+       self.card = card
+       self.value = values[card]
+
+    def str(self):
+        return self.card + 'of' + self.color
