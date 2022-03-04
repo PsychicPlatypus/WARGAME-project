5c761065f87c87b3ab6a2eccada1940d80f9522a
Luan Rexhepi <lu.rexhepi@gmail.com>
2022-03-04 16:30:36 +0100

    Added the comments and the code lines for Card class

--- game/Card.py
+++ game/Card.py
@@ -2,6 +2,7 @@
 import random
 
 class Card:
+    #Declaring the deck of cards and their values
     suits = ['Hearts','Clubs','Spades','Diamonds']
 
     ranks = ['Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King','Ace']
@@ -9,10 +10,12 @@ class Card:
     values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six' : 6 , 'Seven' :7, 'Eight': 8,'Nine':9,
           'Ten':10,'Jack':11, 'Queen': 12,'King': 13,'Ace':14 }
     
-    def random_card():
-        random.shuffle(ranks)
-        random.shuffle(suits)
-        return ranks[0] + " of " + suits[0]
-
-    def value_of_card(input):
-        return values.get(input.split(" of ")[0])
+    #Function to randomaize the lists and shuffling the cards
+    def random_card(self):
+        random.shuffle(self.ranks)
+        random.shuffle(self.suits)
+        return self.ranks[0] + " of " + self.suits[0]
+    
+    #Function ot get the value of the cards
+    def value_of_card(self, value):
+        return self.values.get(value.split(" of ")[0])
