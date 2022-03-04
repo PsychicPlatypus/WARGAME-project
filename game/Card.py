2d7c654aff48968f411567a6a245f6a28d1467d5
Luan Rexhepi <lu.rexhepi@gmail.com>
2022-03-04 01:08:36 +0100

    Added the code for the Card class

--- game/Card.py
+++ game/Card.py
@@ -1,16 +1,18 @@
 # Assigned to Luan
-colors = ['Hearts','Clubs','Spades','Diamonds']
-
-cards = ['Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King','Ace']
-
-values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six' : 6 , 'Seven' :7, 'Eight': 8,'Nine':9,
-          'Ten':10,'Jack':11, 'Queen': 12,'King': 13,'Ace':14 }
+import random
 
 class Card:
-    def init(self, color, card):
-       self.color = color
-       self.card = card
-       self.value = values[card]
+    suits = ['Hearts','Clubs','Spades','Diamonds']
 
-    def str(self):
-        return self.card + 'of' + self.color
+    ranks = ['Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King','Ace']
+
+    values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six' : 6 , 'Seven' :7, 'Eight': 8,'Nine':9,
+          'Ten':10,'Jack':11, 'Queen': 12,'King': 13,'Ace':14 }
+    
+    def random_card():
+        random.shuffle(ranks)
+        random.shuffle(suits)
+        return ranks[0] + " of " + suits[0]
+
+    def value_of_card(input):
+        return values.get(input.split(" of ")[0])
